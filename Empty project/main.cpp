69baaad3d9c315a1b99e9bdd6bb53fb7937ba166
taplonaplo <taplonaplo@gmail.com>
2016-07-27 13:57:32 +0200

    Added basic SDL Window

--- Empty project/main.cpp
+++ Empty project/main.cpp
@@ -1,8 +1,35 @@
 #include "stdio.h"
+#include "SDL.h"
 
+/*#ifdef _WIN32
+#undef main
+#endif*/
 
-int main(int argc, void* argv) {
+int main(int argc, char* argv[]) {
+	SDL_Window *window;                    // Declare a pointer
+
+	SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+										   // Create an application window with the following settings:
+	window = SDL_CreateWindow(
+		"An SDL2 window",                  // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		640,                               // width, in pixels
+		480,                               // height, in pixels
+		SDL_WINDOW_OPENGL                  // flags - see below
+	);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
 	printf("Hello World!");
 	getchar();
+
+
 	return 0;
 }
