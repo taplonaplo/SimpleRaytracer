fb0f49feba46f0ee3fc93fee29f3f63756d3f5e5
taplonaplo <taplonaplo@gmail.com>
2016-07-31 22:24:40 +0200

    variable renamied

--- Empty project/Camera.h
+++ Empty project/Camera.h
@@ -19,14 +19,14 @@ public:
 		, up(up)
 		, right(right)
 		, fieldOfView(fieldOfView)
-	{
-	}
+	{ }
+
 	Ray GetRay(int x, int y, int width, int height)
 	{
 		float aspectRatio = width > height ? width / (float)height : height / (float)width;
 		float yOffset = 2.f * (y - height / 2) * tanf(fieldOfView / 2.f * glm::pi<float>() / 180.f);
 		float xOffset = 2.f * (x - width / 2)  * tanf(fieldOfView / 2.f * glm::pi<float>() / 180.f);
-		glm::vec3 target = (lookAt + up * yOffset + right * xOffset) - eye;
-		return Ray(eye, glm::normalize(target));
+		glm::vec3 direction = (lookAt + up * yOffset + right * xOffset) - eye;
+		return Ray(eye, glm::normalize(direction));
 	}
 };
